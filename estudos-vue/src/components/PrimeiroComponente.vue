89c5bfa9bddcb42f371d3e9ae8021767c5af1bb7
Marcelo Joras <marcelojoras@hotmail.com>
2022-11-14 17:34:10 -0300

    Primeiro componente

---
+++ estudos-vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Olá Vue!</h1>
+    <p>Meu nome é: {{nome}}</p>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                nome: 'Marcelo'
+            }
+        }
+    }
+</script>
