8bbe0edffe292b087cee0db61d9e052c74d8c4c8
Marcelo Joras <marcelojoras@hotmail.com>
2022-11-18 15:07:12 -0300

    Banner

--- make-order-project/src/views/HomeView.vue
+++ make-order-project/src/views/HomeView.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="main-container">
-    <h1>Home</h1>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte o seu pedido</h1>
+      <p>Formulário</p>
+    </div>
   </div>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue'
+
+export default {
+  components: {
+    Banner
+  }
+}
 </script>
