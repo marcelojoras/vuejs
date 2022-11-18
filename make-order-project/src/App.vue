56979387733c13fa13df940517e0b1c489939dd7
Marcelo Joras <marcelojoras@hotmail.com>
2022-11-18 14:46:45 -0300

    Header e Footer

--- make-order-project/src/App.vue
+++ make-order-project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NavBar />
+    <NavBar :logo="logoSrc" :alt="appName" />
     <router-view/>
     <Footer />
   </div>
@@ -13,10 +13,33 @@
     components: {
       NavBar,
       Footer
+    },
+    data() {
+      return {
+        logoSrc: '/img/logo.png',
+        appName: 'Make order'
+      }
     }
   }
 </script>
 
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
